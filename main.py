984fc3334ca8d7937b33914670cb29a103a06c92
kafka6666 <kafihrvrd100@gmail.com>
2025-02-15 14:07:50 +0600

    add bright data scrape browser configuration

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import streamlit as st
+from scrape import scrape_website
 
 # page title
 st.title("AI Web Scraper")
 url = st.text_input("Enter the Website URL: ")
 
+# scrape button functionality
 if st.button("Scrape Site"):
     st.write("Scraping the site...")
-
+    scraped_content = scrape_website(url)
+    print(scraped_content)
--- scrape.py
+++ scrape.py
@@ -1,21 +1,26 @@
-import selenium.webdriver as webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver import Remote, ChromeOptions
+from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
+from bs4 import BeautifulSoup
+
+
+AUTH = 'brd-customer-hl_2b09caf9-zone-scraping_browser2:mfaeshtz1gdj'
+SBR_WEBDRIVER = f'https://{AUTH}@brd.superproxy.io:9515'
 
 def scrape_website(website_url):
-    print("Launching Chrome Browser...")
-
-    chrome_driver_path = ""
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
-
-    try:
+    print('Connecting to Scraping Browser...')
+    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
+    with Remote(sbr_connection, options=ChromeOptions()) as driver:
+        print('Connected! Navigating...')
         driver.get(website_url)
-        print("Page Loaded Successfully!")
+        print('Navigated! Scraping page content...')
         html = driver.page_source
-        print("HTML Extracted Successfully!")
+        print("html extracted successfully")
         return html
-        
-    except Exception as e:
-        print(f"Error while getting the site content: {e.__str__()}")
-    finally:
-        driver.quit()
+
+
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    body_content = soup.body
+    if body_content:
+        return body_content
+    return ""
